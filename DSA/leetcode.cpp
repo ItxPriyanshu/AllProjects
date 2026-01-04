fb0133e48b748688f6ddb58e89c805fd0b529f58
ItxPriyanshu <priyanshukumar3644@gmail.com>
2026-01-04 09:51:25 +0530

    leetcode 1390 solved

--- DSA/leetcode.cpp
+++ DSA/leetcode.cpp
@@ -1,12 +1,66 @@
-// 
+// 1390. Four Divisors
 /*
-You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colors: Red, Yellow, or Green
-while making sure that no two adjacent cells have the same color (i.e., no two cells that share vertical or horizontal sides have the same
- color).
-Given n the number of rows of the grid, return the number of ways you can paint this grid. 
-As the answer may grow large, the answer must be computed modulo 109 + 7.
+Given an integer array nums, return the sum of divisors of the integers in that array that
+have exactly four divisors. If there is no such integer in the array, return 0.
 */
 
 #include<bits/stdc++.h>
 using namespace std;
+
+//1st approach brute force(TLE)
+/*
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int count = 0;
+            int temp = 0;
+            for (int j = 1; j <= nums[i]; j++) {
+                if (nums[i] % j == 0) {
+                    count++;
+                    temp += j;
+                }
+            }
+            if (count == 4) {
+                ans += temp;
+            }
+        }
+        return ans;
+    }
+};*/
+
+
+//optimized solution
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for (int n : nums) {
+            int count = 0;
+            int temp = 0;
+            for (int j = 1; j * j <= n; j++) {
+                if (n % j == 0) {
+                    int d1 = j;
+                    int d2 = n / j;
+
+                    if (d1 == d2) {
+                        count += 1;
+                        temp += d1;
+                    } else {
+                        count += 2;
+                        temp += d1 + d2;
+                    }
+
+                    if (count > 4)
+                        break;
+                }
+            }
+            if (count == 4) {
+                ans += temp;
+            }
+        }
+        return ans;
+    }
+};
 
